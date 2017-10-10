5b742e2b801f21e94370eb1daf44c4005752270f
riskteria <riskteria@gmail.com>
2017-10-10 12:28:55 +0700

    remove secret

--- app/src/main/java/com/example/riskteria/kotlinlistanko/AppConfig.kt
+++ app/src/main/java/com/example/riskteria/kotlinlistanko/AppConfig.kt
@@ -6,6 +6,5 @@ package com.example.riskteria.kotlinlistanko
 
 object AppConfig {
     val LASTFM_API_KEY: String = "c5166caa30b6c56455f7e1dcc54eb83b"
-    val LASTFM_SHARED_SECRET: String = "7cdec3fb1e2c69b07a073ce629c7d8d1"
     val LASTFM_API_URL: String = "http://ws.audioscrobbler.com"
 }
